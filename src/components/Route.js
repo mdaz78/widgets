f724d06f351ddef85dea2e6b6d4a0db7275f61e6
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-12-27 01:02:12 +0530

    Fix route
    - Make navigation work
    - Make the default behavior of cmd or ctrl plus click work

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,3 +1,16 @@
+import { useEffect, useState } from 'react';
+
 export default function Route({ path, children }) {
-  return window.location.pathname === path ? children : null;
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => setCurrentPath(window.location.pathname);
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
 }
